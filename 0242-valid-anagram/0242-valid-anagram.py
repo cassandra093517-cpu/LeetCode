95bc7652d8b5107be62e2ed7a2ead6939ef05d09
Cassandra Kao <cassandra093517@gmail.com>
2026-09-15 17:14:43 -0400

    Time: 8 ms (83.12%), Space: 19.4 MB (46.1%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,23 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        
-        if len(s)!=len(t):
-            return False
-        
-        sNum, tNum={},{}
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        res=dict()
 
-        for i in range(len(s)):
-            sNum[s[i]] = 1+sNum.get(s[i],0)
-            tNum[t[i]] = 1+tNum.get(t[i],0)
+        for x in s:
+            if x in res:
+                res[x]+=1
+            else:
+                res[x]=1
         
-        for c in sNum:
-            if sNum[c]!= tNum.get(c,0):
+        for x in t:
+            if x in res:
+                res[x]-=1
+            else: return False
+        
+        for x in res.values():
+            if x !=0:
                 return False
+
+        return True  
+
         
-        return True
+        
