9984a024b0278b31332dd5a4d02543be9a197132
Cassandra Kao <cassandra093517@gmail.com>
2026-09-15 16:44:47 -0400

    Time: 0 ms (100%), Space: 20.5 MB (41.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        numList={}
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+
+        output = dict()
 
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in numList:
-                return [numList[diff],i]
-            numList[n]=i
-        return []
+            diff =  target-n
+            if diff in output:
+                return [output[diff],i]
+            
+            output[n]=i
